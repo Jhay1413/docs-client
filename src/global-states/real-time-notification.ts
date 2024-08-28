3610dc4a22b8f2da117fff8922714e39b8690b9f
jhon <jhon041413@gmail.com>
2024-08-28 14:23:42 +0800

    wip

--- src/global-states/real-time-notification.ts
+++ src/global-states/real-time-notification.ts
@@ -1,26 +1,36 @@
-import {create} from 'zustand';
-import { io, Socket } from 'socket.io-client';
-import { toast } from 'react-toastify';
+import { create } from "zustand";
+import { io, Socket } from "socket.io-client";
+import { toast } from "react-toastify";
 
 interface NotificationState {
   messages: string[];
   addNotification: (message: string) => void;
   socket: Socket;
   initializeSocket: () => void;
-  receiverId:string | null;
-  setReceiverId:(id: string) => void;
+  receiverId: string | null;
+  setReceiverId: (id: string) => void;
 }
 
-const useRealtimeStore = create<NotificationState>((set) => ({
-  socket: io("https://docs-api-9r6n.onrender.com"),
-  //socket: io("http://localhost:3001"), 
-  receiverId:null,// Initialize the socket here
-  initializeSocket: () => set((state) => ({
-    socket: io("https://docs-api-9r6n.onrender.com"), // Reinitialize if needed
-  })),
-  messages: [],
-  addNotification: (message) => set((state) => ({ messages: [...state.messages, message] })),
-  setReceiverId : ()=>set((state)=>({receiverId:state.receiverId}))
-}));
+const useRealtimeStore = create<NotificationState>((set) => {
+  const mode = import.meta.env.VITE_MODE;
+
+  const backendUrl =
+    mode == "DEVELOPMENT"
+      ? import.meta.env.VITE_PROD_API
+      : import.meta.env.VITE_DEV_API;
+  return {
+    socket: io(backendUrl),
+    //socket: io("http://localhost:3001"),
+    receiverId: null, // Initialize the socket here
+    initializeSocket: () =>
+      set((state) => ({
+        socket: io(backendUrl), //Reinitialize if needed
+      })),
+    messages: [],
+    addNotification: (message) =>
+      set((state) => ({ messages: [...state.messages, message] })),
+    setReceiverId: () => set((state) => ({ receiverId: state.receiverId })),
+  };
+});
 
 export default useRealtimeStore;
