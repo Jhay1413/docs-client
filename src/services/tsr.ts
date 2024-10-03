ae4a8706cdf78fd72c40bf18196e132883f4f6e7
Jhon <jhon041413@gmail.com>
2024-10-03 11:37:28 +0800

    wip

--- src/services/tsr.ts
+++ src/services/tsr.ts
@@ -1,9 +1,9 @@
 import { initTsrReactQuery } from "@ts-rest/react-query/v5";
 import { companyContract, contracts, transactionContract } from "shared-contract";
-const mode = import.meta.env.VITE_MODE;
-const dev = import.meta.env.VITE_DEV_API;
-const prod = import.meta.env.VITE_PROD_API;
-const local = import.meta.env.VITE_LOCAL_API;
+// const mode = import.meta.env.VITE_MODE;
+// const dev = import.meta.env.VITE_DEV_API;
+// const prod = import.meta.env.VITE_PROD_API;
+// const local = import.meta.env.VITE_LOCAL_API;
 export const tsr = initTsrReactQuery(contracts, {
-  baseUrl: mode == "PROD" ? prod : mode === "DEV" ? dev : local,
+  baseUrl: "https://docs-api-wrvs.onrender.com",
 });
