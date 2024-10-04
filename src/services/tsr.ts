8373bb84701129c4f0fc5ca2be11f743eb0ec3c9
Jhon <jhon041413@gmail.com>
2024-10-04 08:37:23 +0800

    fix some bugs on fetching transaction

--- src/services/tsr.ts
+++ src/services/tsr.ts
@@ -1,9 +1,10 @@
 import { initTsrReactQuery } from "@ts-rest/react-query/v5";
 import { companyContract, contracts, transactionContract } from "shared-contract";
 // const mode = import.meta.env.VITE_MODE;
-// const dev = import.meta.env.VITE_DEV_API;
+const endpoint = import.meta.env.VITE_ENDPOINT;
 // const prod = import.meta.env.VITE_PROD_API;
 // const local = import.meta.env.VITE_LOCAL_API;
 export const tsr = initTsrReactQuery(contracts, {
-  baseUrl: "https://dts-dev.onrender.com",
+  // baseUrl: "https://dts-dev.onrender.com",
+  baseUrl: endpoint,
 });
