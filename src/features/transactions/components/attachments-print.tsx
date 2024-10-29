b405082bb25631580031a42b91385e04b817cabf
Jhon <jhon041413@gmail.com>
2024-10-29 14:18:05 +0800

    wip

--- src/features/transactions/components/attachments-print.tsx
+++ src/features/transactions/components/attachments-print.tsx
@@ -3,6 +3,14 @@ import { completeStaffWorkQuerySchema, filesQuerySchema } from "shared-contract"
 import { z } from "zod";
 
 export const AttachmentOnPrint = ({ attachments }: { attachments: z.infer<typeof completeStaffWorkQuerySchema>[] }) => {
+  const mutateData = attachments.map((data) => {
+    const id = data.id;
+    const urls = data.attachments.map((data) => {
+      return { url: data };
+    });
+    return { id, data: urls };
+  });
+
   return (
     <div className="flex flex-col gap-4 bg-black ">
       <div className="flex flex-col "></div>
