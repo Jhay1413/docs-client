b56b4974db39aa74d498e79cc269f5cf3b7a971c
lourinsi17 <19100121@usc.edu.ph>
2024-11-07 10:51:04 +0800

    fixed column, tables, sidenavs etc.

--- src/features/transactions/components/table-data/archieved-list.tsx
+++ src/features/transactions/components/table-data/archieved-list.tsx
@@ -55,57 +55,62 @@ export const ArchivedList = () => {
     navigate(`/dashboard/transactions/history/${data.id}`);
   };
   return (
-    <div className="flex flex-col ">
+    <div className="min-h-full flex flex-col w-full items-center p-4 bg-white rounded-lg ">
       <div className="flex flex-col w-full items-center justify-center p-4 bg-white rounded-lg">
-        <div className="flex justify-start w-full flex-col ">
-          <h1 className="text-[#404041] font-medium text-[28px]">List of Archive Transactions</h1>
-          <p className="text-muted-foreground text-[12px]">View and manage all past transactions stored in the archive.</p>
+        <div className="flex justify-between items-center w-full pb-4">
+          <div className="flex justify-start w-full flex-col ">
+            <h1 className="text-[#404041] font-medium text-[28px]">List of Archive Transactions</h1>
+            <p className="text-muted-foreground text-[12px]">View and manage all past transactions stored in the archive.</p>
+          </div>
+        
+          <div className="flex items-center justify-end w-full ">
+            <Input
+              placeholder="Search ...."
+              defaultValue={debouncedSearchQuery}
+              onChange={(e) =>
+                setSearchParams(
+                  (prev) => {
+                    prev.set("search", e.target.value);
+                    prev.set("currentPage", "1");
+                    return prev;
+                  },
+                  { replace: true },
+                )
+              }
+              className="w-[289px] rounded-none  rounded-l-md"
+            />
+            <button className="p-2 bg-primaryColor text-white rounded-r-md ">
+              <Search />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center py-4 justify-end w-full ">
-        <Input
-          placeholder="Search ...."
-          defaultValue={debouncedSearchQuery}
-          onChange={(e) =>
-            setSearchParams(
-              (prev) => {
-                prev.set("search", e.target.value);
-                prev.set("currentPage", "1");
-                return prev;
-              },
-              { replace: true },
-            )
-          }
-          className="w-[289px] rounded-none  rounded-l-md"
-        />
-        <button className="p-2 bg-primaryColor text-white rounded-r-md ">
-          <Search />
-        </button>
-      </div>
+
       <DataTable columns={archivedColumn} data={searchData ? searchData.body.data! : []} callbackFn={handleOnClickRow} />
-      <div className="flex items-center justify-between w-full  space-x-2 py-4">
-        <div className="text-muted-foreground">
-          <h1>Number of Transactions: {searchData?.body.numOfTransactions}</h1>
+        <div className="w-full flex justify-between items-center">
+          <div className="text-muted-foreground">
+            <h1>Number of Transactions: {searchData?.body.numOfTransactions}</h1>
+          </div>
+          <div className="flex items-center space-x-2 py-4">
+            <Button variant="outline" size="sm">
+              {"<<"}
+            </Button>
+            <Button variant="outline" size="sm" onClick={handlePreviousPage} disabled={intPage == 1}>
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={handleNextPage}
+              // Disable if on the first page
+            >
+              Next
+            </Button>
+            <Button variant="outline" size="sm">
+              {">>"}
+            </Button>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <Button variant="outline" size="sm">
-            {"<<"}
-          </Button>
-          <Button variant="outline" size="sm" onClick={handlePreviousPage} disabled={intPage == 1}>
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={handleNextPage}
-            // Disable if on the first page
-          >
-            Next
-          </Button>
-          <Button variant="outline" size="sm">
-            {">>"}
-          </Button>
-        </div>
+        
       </div>
     </div>
   );
