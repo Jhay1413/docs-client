285cf4fa1219abf0afaf6aefa2b1cf3dab1d9d1d
Andre <jaguardian00@gmail.com>
2024-11-12 16:22:16 +0800

    Added tests for trasnaction table

--- cypress/e2e/transactions.cy.ts
+++ cypress/e2e/transactions.cy.ts
@@ -22,21 +22,33 @@ describe('Transactions Test', () => {
       cy.get('[data-cy="trans-search"]').should('be.visible');
     });
 
-    it('should access transaction table', () => {
+    it('should access transaction table columns', () => {
       cy.visit('http://localhost:5173/dashboard/transactions/list');
-      cy.get('[data-cy="trans-table"]').should('be.visible');
-    })
-  
-    // it('should access transactions table', () => {
-    //   cy.get('th:contains("Company Name")').should('be.visible');
-    //   cy.get('th:contains("Project Name")').should('be.visible');
-    //   cy.get('th:contains("Type")').should('be.visible');
-    //   cy.get('th:contains("Subtype")').should('be.visible');
-    //   cy.get('th:contains("Subject")').should('be.visible');
-    //   cy.get('th:contains("Forwarder")').should('be.visible');
-    //   cy.get('th:contains("Receiver")').scrollIntoView().should('be.visible');
-    //   cy.get('th:contains("Status")').should('be.visible');
-    //   cy.get('th:contains("Priority")').should('be.visible');
-    //   cy.get('th:contains("Percentage")').should('be.visible');
-    // });
+      cy.get('th:contains("Transaction ID")').should('be.visible');
+      cy.get('th:contains("Company Name")').should('be.visible');
+      cy.get('th:contains("Project Name")').should('be.visible');
+      cy.get('th:contains("Type")').should('be.visible');
+      cy.get('th:contains("Subtype")').should('be.visible');
+      cy.get('th:contains("Subject")').should('be.visible');
+      cy.get('th:contains("Forwarder")').should('be.visible');
+      cy.get('th:contains("Receiver")').scrollIntoView().should('be.visible');
+      cy.get('th:contains("Status")').should('be.visible');
+      cy.get('th:contains("Priority")').should('be.visible');
+      cy.get('th:contains("Percentage")').should('be.visible');
+    });
+
+    it('should access transaction table data', () => {
+      cy.visit('http://localhost:5173/dashboard/transactions/list');
+      cy.get('td:contains("Transaction ID")').should('be.visible');
+      cy.get('td:contains("Company Name")').should('be.visible');
+      cy.get('td:contains("Project Name")').should('be.visible');
+      cy.get('td:contains("Type")').should('be.visible');
+      cy.get('td:contains("Subtype")').should('be.visible');
+      cy.get('td:contains("Subject")').should('be.visible');
+      cy.get('td:contains("Forwarder")').should('be.visible');
+      cy.get('td:contains("Receiver")').scrollIntoView().should('be.visible');
+      cy.get('td:contains("Status")').should('be.visible');
+      cy.get('td:contains("Priority")').should('be.visible');
+      cy.get('td:contains("Percentage")').should('be.visible');
+    });
  });
