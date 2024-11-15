2a3ceda1b7249eaeb9ef9b9c2249dd672a415e7e
lourinsi17 <19100121@usc.edu.ph>
2024-11-15 13:06:45 +0800

    ticketcolumn route

--- src/features/tickets/components/tables/ticket-inbox.tsx
+++ src/features/tickets/components/tables/ticket-inbox.tsx
@@ -11,6 +11,7 @@ import { getCurrentUserId } from "@/hooks/use-user-hook";
 import { FilterOptions } from "../filter-options";
 
 export const TicketInboxComponent = () => {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams({
     currentPage: "1",
     search: "",
@@ -85,6 +86,12 @@ export const TicketInboxComponent = () => {
     }
   };
 
+  const handleOnClickRow = (data: any) => {
+    // Navigate to ticket details page when a row is clicked
+    // navigate(`/dashboard/tickets/details/${data.id}`);
+  };
+
+  
   return (
     <div className="min-h-full flex flex-col w-full items-center p-4 bg-white rounded-lg">
       <div className="flex flex-col w-full items-center justify-center p-4 bg-white rounded-lg">
@@ -129,7 +136,7 @@ export const TicketInboxComponent = () => {
           </div>
         </div>
         
-        <DataTable columns={ticketsInboxColumn} data={data ? data.body.data : []} />
+        <DataTable columns={ticketsInboxColumn} data={data ? data.body.data : []} callbackFn={handleOnClickRow} />
 
         <div className="w-full flex justify-between items-center">
           <div className="text-muted-foreground">
