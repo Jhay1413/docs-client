1e5d1291f3fa76a0ea730605351f345d6e403508
lourinsi17 <19100121@usc.edu.ph>
2024-11-18 10:06:44 +0800

    removed admin sidenav

--- src/layout/Sidenav.tsx
+++ src/layout/Sidenav.tsx
@@ -263,36 +263,6 @@ export const SideNav = () => {
             </NavLink>
           </li>
         </ul>
-        <Label className="text-[#DCFF8E] px-4 font-medium flex w-full text-sm">ADMIN</Label>
-        <ul className="flex flex-col  space-y-4 w-full   mx-2 ">
-          <li className="relative inline-block text-left px-4 ">
-            <NavLink
-              to={`/dashboard/transactions/archived`}
-              className={({ isActive }) => {
-                return `justify-start items-center flex w-full p-2 space-x-4 text-lg  rounded-md ${isActive ? "bg-green-100/30 text-white" : ""}`;
-              }}
-            >
-              <FileCode />
-              <div className="flex gap-2">
-                <h1 className="text-base">Archives</h1>
-              </div>
-            </NavLink>
-          </li>
-
-          <li className="relative inline-block text-left px-4 ">
-            <NavLink
-              to={`/dashboard/tickets/resolved-tickets`}
-              className={({ isActive }) => {
-                return `justify-start items-center flex w-full p-2 space-x-4 text-lg  rounded-md ${isActive ? "bg-green-100/30 text-white" : ""}`;
-              }}
-            >
-              <BookCheck />
-              <div className="flex gap-2">
-                <h1 className="text-base">Resolved Tickets</h1>
-              </div>
-            </NavLink>
-          </li>
-        </ul>
       </div>
     </div>
   );
