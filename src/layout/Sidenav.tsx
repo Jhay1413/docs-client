0658705b6108bf2a388c23f7f132b13345844545
Andre <jaguardian00@gmail.com>
2024-11-19 14:51:22 +0800

    Fixed fetching of projects and transactions to add limit and offset

--- src/layout/Sidenav.tsx
+++ src/layout/Sidenav.tsx
@@ -99,6 +99,22 @@ export const SideNav = () => {
           </li>
           <CompaniesItemWithRole roles={["SUPERADMIN", "MANAGER"]} exemptions={["Operations Department"]} />
         </ul>
+        <Label className="text-[#DCFF8E] px-4 font-medium flex w-full text-sm">ADMIN</Label>
+          <ul className="flex flex-col  space-y-4 w-full   mx-2 ">
+            <li className="relative inline-block text-left px-4 ">
+              <NavLink
+                to={`/dashboard/admin/request`}
+                className={({ isActive }) => {
+                  return `justify-start items-center flex w-full p-2 space-x-4 text-lg  rounded-md ${isActive ? "bg-green-100/30 text-white" : ""}`;
+                }}
+              >
+                <FilePen />
+                <div className="flex gap-2">
+                  <h1 className="text-base">Request Forms</h1>
+                </div>
+              </NavLink>
+          </li>
+        </ul>
         <UserAndAccountWithRole roles={["SUPERADMIN"]} exemptions={["Operations Department"]} />
         <Label className="text-[#DCFF8E] px-4 font-medium flex w-full text-sm">TRANSACTIONS</Label>
         <ul className="flex flex-col  space-y-4 w-full   mx-2">
@@ -279,21 +295,6 @@ export const SideNav = () => {
                 <h1 className="text-base">User Manual</h1>
               </div>
             </NavLink>
-
-        <Label className="text-[#DCFF8E] px-4 font-medium flex w-full text-sm">ADMIN</Label>
-          <ul className="flex flex-col  space-y-4 w-full   mx-2 ">
-            <li className="relative inline-block text-left px-4 ">
-              <NavLink
-                to={`/dashboard/admin/request`}
-                className={({ isActive }) => {
-                  return `justify-start items-center flex w-full p-2 space-x-4 text-lg  rounded-md ${isActive ? "bg-green-100/30 text-white" : ""}`;
-                }}
-              >
-                <FilePen />
-                <div className="flex gap-2">
-                  <h1 className="text-base">Request Forms</h1>
-                </div>
-              </NavLink>
           </li>
         </ul>
       </div>
