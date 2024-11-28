25f8800dccbd1cf8d3ef63c1752950796f3af5e1
Andre <jaguardian00@gmail.com>
2024-11-28 13:24:55 +0800

    Added collapsible sections for the sidenav

--- src/global-states/notification-store.ts
+++ src/global-states/notification-store.ts
@@ -28,3 +28,32 @@ export const useNotificationStore = create<NotificationState>((set) => ({
   setNotification: (notification) => set({ notification }),
   setRefetch: (refetchFn) => set({ refetch: refetchFn }),
 }));
+
+export type TicketNotificationType = {
+  incomingTickets: number,
+  inboxTickets: number,
+  refetch?: () => void;
+  refetchAll?: () => void;
+}
+
+export type TicketNotificationState = {
+  ticketNotification: TicketNotificationType | null;
+  numOfUnreadTicketNotif: number;
+  setNumOfUnreadTicketNotif: (numOfUnreadTicketNotif: number) => void;
+  setTicketNotification: (ticketNotification: TicketNotificationType | null) => void;
+  refetch: (() => void) | null;
+  refetchAll: (() => void) | null;
+  setRefetchAll: (refetchFn: () => void) => void;
+  setRefetch: (refetchFn: () => void) => void;
+}
+
+export const useTicketNotificationStore = create<TicketNotificationState>((set) => ({
+  ticketNotification: null,
+  numOfUnreadTicketNotif: 0,
+  refetch: null,
+  refetchAll: null,
+  setNumOfUnreadTicketNotif: (numOfUnreadTicketNotif) => set({ numOfUnreadTicketNotif }),
+  setRefetchAll: (refetchFn) => set({ refetchAll: refetchFn }),
+  setTicketNotification: (ticketNotification) => set({ ticketNotification }),
+  setRefetch: (refetchFn) => set({ refetch: refetchFn }),
+}));
